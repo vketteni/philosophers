52de16a81f3a7d5d431901850320d75f6be071a0
Vincent Ketteniss <vketteni@c3a10c5.42berlin.de>
2024-05-01 06:56:40 +0200

    remove libft

---
+++ philo/philo.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vketteni <vketteni@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:11:34 by vketteni          #+#    #+#             */
+/*   Updated: 2024/04/29 11:11:37 by vketteni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+# ifndef	PHILO_H
+# define	PHILO_H
+
+# include "../libft/libft.h"
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+
+
+/* 
+	Allowed functions:
+		memset, printf, malloc, free, write,
+		usleep, gettimeofday, pthread_create,
+		pthread_detach, pthread_join, pthread_mutex_init,
+		pthread_mutex_destroy, pthread_mutex_lock,
+		pthread_mutex_unlock
+ */
+
+# endif
